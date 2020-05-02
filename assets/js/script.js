8e42a37f288caad9c8e625ac347bbf84bd153a67
nazmul2666 <60926653+nazmul2666@users.noreply.github.com>
2020-05-02 22:47:40 +0530

    added subscribe button & random api picker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,33 @@
+var myKeys = ["AIzaSyC1Dz3mDyUn7ftZ7Gaj36ILi_IzkR1jnEY","AIzaSyA4dpfufukRsnCNBYczWDLSkXX3iPfFH3Q","AIzaSyApymRb4-0SGDMfgVgqTGPj1TyLxDtvLXA","AIzaSyBsPJIa4Sut3yHlHpbJChNKUUA7UY8QejI","AIzaSyBc-iXWRHwYc4UHFxKcqPhc9DwZmrXQ2Zs","AIzaSyASZuqWAdJOcQvJ5dm_1rHODBzPkOBFtcw","AIzaSyDY_3PDTiIW451bBxwHlyNiz8lLFrkwgx0","AIzaSyDw02S2fHH7WjXtCH-SfRrzyeMGLGbsXzw","AIzaSyDLfowqkDy_W7vDJXfbR6QuG1iQmwhcAUg"];
+var key = myKeys[Math.floor(Math.random() * myKeys.length)];
+
+
+
 $(document).ready( function() {
-        
+
     var chanName = "";
-    loadChannel("UCq-Fj5jknLsUf-MWSy4_brA");
+    loadChannel('UCAW-NpUFkMyCNrvRSSGIvDQ');
     function loadChannel(name) {
 
         chanName = name;
 
-        var url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id='+name+'&key=AIzaSyC1Dz3mDyUn7ftZ7Gaj36ILi_IzkR1jnEY';
+        var url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id='+name+'&key='+ key;
         $.getJSON(url, function(data) {
             $('#ytSubs').html(data.items[0].statistics.subscriberCount);
             $('#ytVideo').html(data.items[0].statistics.videoCount);
             $('#ytView').html(data.items[0].statistics.viewCount);
         });
 
-        var url1 = 'https://www.googleapis.com/youtube/v3/channels?part=snippet&id=' + name + '&key=AIzaSyC1Dz3mDyUn7ftZ7Gaj36ILi_IzkR1jnEY';
+        var url1 = 'https://www.googleapis.com/youtube/v3/channels?part=snippet&id='+name+'&key='+key;
         $.getJSON(url1, function (data1) {
             $('#ytName').html(data1.items[0].snippet.title);
             $('#ytImage').html('<img src="' + data1.items[0].snippet.thumbnails.medium.url + '">');
-            $('#ytLink').html('<a href=\"https://www.youtube.com/channels' + data1.items[0].snippet.id + '\">Link</a>');
+            $('#ytLink').html('<a href=\"https://www.youtube.com/channel/' +name+'?sub_confirmation=1">Subscribe</a>');
         });
     }
 
     setInterval(function () {
-        var url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=' + chanName + '&key=AIzaSyC1Dz3mDyUn7ftZ7Gaj36ILi_IzkR1jnEY';
+        var url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id='+chanName+'&key='+keyManager.key;
         $.getJSON(url, function (data) {
             $('#ytSubs').html(data.items[0].statistics.subscriberCount);
             $('#ytVideo').html(data.items[0].statistics.videoCount);
